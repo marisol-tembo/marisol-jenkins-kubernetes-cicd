f88daed7c2135c5d9c762cd6c8ef54186d5ab9a9
Dania Marisol Ucan Gongora <marisolucangongora@Danias-MacBook-Pro.local>
2026-09-06 17:02:18 -0600

    editing the index.html

--- app/src/main/java/com/marisol/demo/HealthController.java
+++ app/src/main/java/com/marisol/demo/HealthController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-  @GetMapping("/")
-  public Map<String, String> home() {
+  @GetMapping("/health")
+  public Map<String, String> health() {
+    return Map.of("status", "UP");
+  }
+
+  @GetMapping("/api/info")
+  public Map<String, String> info() {
     return Map.of(
         "service", "jenkins-k8s-demo-app",
         "status", "ok",
         "message", "Hello from Jenkins -> Ansible -> EKS"
     );
-  }
-
-  @GetMapping("/health")
-  public Map<String, String> health() {
-    return Map.of("status", "UP");
   }
 }
